6bdfb235ce4ca90d46d9300a1a5ae1bf656ffdfa
Cassidy Tarng <catarng@cisco.com>
2019-03-07 13:24:26 -0800

    DB Creation and Login working

---
+++ DBCreation.py
@@ -0,0 +1,81 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db = client.foresite
+cursor = db.user.find()
+
+# This exits if there already exists a user in the collection.
+# This will prevent you from creating duplicate users if you run this script multiple times
+if len(list(cursor)) > 0:
+    exit()
+
+for doc in cursor:
+    print(doc)
+
+# Insert user
+query = {
+    'user_id': 'U123',
+    'user_name': 'ctarng',
+    'first_name': 'Cassidy',
+    'last_name': 'Tarng',
+    'email': 'ctarng@scu.edu',
+    'phone_number': '5106762337',
+    'events_attended': [''],
+    'events_not_attended': [''],
+    'events_published': [''],
+    'events_registered': ['E001'],
+    'event_tickets': ['T123'],
+    'creation_date': 'T123',
+    'password': 'tempPassword'
+}
+
+db.user.insert_one(query)
+print('1 User has been created')
+
+# Insert event
+query = {
+    'event_id': 'E001',
+    'thumbnail_icon': 'icon placeholder',
+    'title': 'Santa Clara Tech Conference',
+    'street': '5001 Great America Pkwy',
+    'city': 'Santa Clara',
+    'state': 'CA',
+    'zip_code': '95054',
+    'start_time': '13:00',
+    'end_time': '20:00',
+    'is_tbd': 0,
+    'category': 'Technology',
+    'description': 'The Santa Clara Tech COnference is a tech conference that....',
+    'max_purchase_quantity': 4,
+    'max_quantity_available': 500,
+    'subtotal_price': 4500,
+    'add_ons': {},
+    'survey_questions': {'survey': [{'question': 'Male or Female', 'answers': ['M', 'F']}]},
+    'event_tickets': ['T123'],
+    'creation_date': '2019-02-20 01:16:21',
+    'last_updated': '2019-02-20 01:16:21'
+}
+
+db.event.insert_one(query)
+print('1 Event has been created')
+
+# Insert event_ticket
+query = {
+    'order_id': 'T123',
+    'user_name': 'ctarng',
+    'user_id': 'U123',
+    'event_id': 'E001',
+    'qr_code': 'QR Code placeholder',
+    'is_ticket_redeemed': 0,
+    'add_ons': {},
+    'survey_questions': {'survey': [{'question': 'Male or Female', 'answers': 'M'}]},
+    'creation_date': '2019-02-20 01:16:21'
+}
+
+db.event_ticket.insert_one(query)
+print('1 Event Ticket has been created')
+
+print('Everything has been created successfully')
+
+
--- Login.py
+++ Login.py
@@ -3,12 +3,14 @@ import pymongo
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient("mongodb://admin:foresiteadmin@54.218.76.138/foresite")  # defaults to port 27017
+#client = pymongo.MongoClient("mongodb://admin:foresiteadmin@54.218.76.138/foresite")  # defaults to port 27017
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client.foresite
 
 @app.route('/foresite/login', methods=['POST'])
 def login():
-
+    print("t")
+    # Check if user_name and password are present in request
     if not request.json or not 'user_name' in request.json or not 'password' in request.json:
         return jsonify({'response': 'fail',
                         'message': 'User name or password is not present'}), 400
@@ -16,9 +18,13 @@ def login():
         'user_name': request.json['user_name'],
         'password': request.json['password']
     }
+
+    # Find with query
     cursor = db.user.find(query)
 
     results = list(cursor)
+
+    # If match found, then return success
     if(len(results) == 1):
         return jsonify({'response': 'success',
                         'message': 'Authentication successful'}), 201
@@ -30,9 +36,17 @@ def login():
 @app.route('/foresite/createUser', methods=['POST'])
 def createUser():
 
-    if not request.json or not 'user_name' in request.json or not 'password' in request.json:
+    # Check if user_name and password are present in request
+    if not request.json or not 'first_name' in request.json \
+            or not 'last_name' in request.json \
+            or not 'email' in request.json \
+            or not 'phone_number' in request.json \
+            or not 'user_name' in request.json \
+            or not 'password' in request.json:
         return jsonify({'response': 'fail',
-                        'message': 'User name or password is not present'}), 400
+                        'message': 'Complete user info not present'}), 400
+
+    # Check if user_name is already taken
     query = {
         'user_name': request.json['user_name']
     }
@@ -43,7 +57,12 @@ def createUser():
         return jsonify({'response': 'fail',
                         'message': 'User name is already being used'}), 400
 
+    # Insert new data
     query = {
+        'first_name':  request.json['first_name'],
+        'last_name': request.json['last_name'],
+        'email': request.json['email'],
+        'phone_number': request.json['phone_number'],
         'user_name': request.json['user_name'],
         'password': request.json['password']
     }
